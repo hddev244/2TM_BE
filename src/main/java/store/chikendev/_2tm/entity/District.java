88d0495ad09658472fdc2e0f38123e89613fd8f5
thanhpvpd <thanhgavip04@gmail.com>
2024-06-11 20:12:02 +0700

    update

--- src/main/java/store/chikendev/_2tm/entity/District.java
+++ src/main/java/store/chikendev/_2tm/entity/District.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -24,7 +22,6 @@ import lombok.NoArgsConstructor;
 @Builder
 public class District {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(length = 50)
--- src/main/java/store/chikendev/_2tm/entity/Ward.java
+++ src/main/java/store/chikendev/_2tm/entity/Ward.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -23,8 +21,8 @@ import lombok.NoArgsConstructor;
 @Table(name = "wards")
 @Builder
 public class Ward {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(length = 50)
     private String name;
