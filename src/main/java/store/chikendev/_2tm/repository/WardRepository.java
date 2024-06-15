1c7e00a6bf5029269efcb3528c1a53a40617ae62
thanhpvpd <thanhgavip04@gmail.com>
2024-06-15 20:09:29 +0700

    update repository

--- src/main/java/store/chikendev/_2tm/repository/WardRepository.java
+++ src/main/java/store/chikendev/_2tm/repository/WardRepository.java
@@ -3,9 +3,9 @@ package store.chikendev._2tm.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import store.chikendev._2tm.entity.Otp;
+import store.chikendev._2tm.entity.Ward;
 
 @Repository
-public interface OtpRepository extends JpaRepository<Otp, String> {
+public interface WardRepository extends JpaRepository<Ward, Long> {
     
 }
