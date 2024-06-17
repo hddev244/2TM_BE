9ad30e661b6e4732fecc59e68197e6ead0fb2501
thanhpvpd <thanhgavip04@gmail.com>
2024-06-18 00:18:38 +0700

    update

--- src/main/java/store/chikendev/_2tm/dto/responce/AddressResponse.java
+++ src/main/java/store/chikendev/_2tm/dto/responce/AddressResponse.java
@@ -1,8 +1,15 @@
 package store.chikendev._2tm.dto.responce;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class AddressResponse {
     private String name;
-
 
 }
