9e74ec4135b8dffbb4c9aca7ba0b4ee376d3d8eb
thanhpvpd <thanhgavip04@gmail.com>
2024-06-18 20:30:39 +0700

    fix

--- src/main/java/store/chikendev/_2tm/dto/request/RequestProduct.java
+++ src/main/java/store/chikendev/_2tm/dto/request/RequestProduct.java
@@ -1,5 +1,14 @@
 package store.chikendev._2tm.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class RequestProduct {
     private String name;
     private Double price;
@@ -9,63 +18,4 @@ public class RequestProduct {
     private Long storeId;
 
     // Constructors, getters, and setters
-
-    public RequestProduct() {}
-
-    public RequestProduct(String name, Double price, Integer quantity, String description, String accountId, Long storeId) {
-        this.name = name;
-        this.price = price;
-        this.quantity = quantity;
-        this.description = description;
-        this.accountId = accountId;
-        this.storeId = storeId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getAccountId() {
-        return accountId;
-    }
-
-    public void setAccountId(String accountId) {
-        this.accountId = accountId;
-    }
-
-    public Long getStoreId() {
-        return storeId;
-    }
-
-    public void setStoreId(Long storeId) {
-        this.storeId = storeId;
-    }
 }
