38b19ff16856211837893703ddf80c5655d75ad3
huanlv100703 <huanlvpd07099@fpt.edu.vn>
2024-06-24 16:23:28 +0700

    task 6 - feat: Lay danh sach gio hang qua userid

--- src/main/java/store/chikendev/_2tm/controller/BillOfLadingController.java
+++ src/main/java/store/chikendev/_2tm/controller/BillOfLadingController.java
@@ -1,39 +1,27 @@
 package store.chikendev._2tm.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
-
-import store.chikendev._2tm.service.BillOfLadingService;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
-import store.chikendev._2tm.dto.request.BillOfLadingRequest;
-import store.chikendev._2tm.dto.responce.ApiResponse;
-import store.chikendev._2tm.dto.responce.BillOfLadingResponse;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class BillOfLadingController {
 
-    @Autowired
-    private BillOfLadingService billOfLadingService;
+    // @Autowired
+    // private BillOfLadingService billOfLadingService;
 
-    @PostMapping("/addBillOfLading")
-    public ApiResponse<BillOfLadingResponse> addBillOfLading(@RequestBody BillOfLadingRequest request) {
-        BillOfLadingResponse response = billOfLadingService.addBillOfLading(request);
-        System.out.println(response);
-        return new ApiResponse<>(200, null, response);
-    }
+    // @PostMapping("/addBillOfLading")
+    // public ApiResponse<BillOfLadingResponse> addBillOfLading(@RequestBody
+    // BillOfLadingRequest request) {
+    // BillOfLadingResponse response = billOfLadingService.addBillOfLading(request);
+    // System.out.println(response);
+    // return new ApiResponse<>(200, null, response);
+    // }
 
-    @GetMapping("/shipList/{deliveryPerson}")
-    public ApiResponse<List<BillOfLadingResponse>> getShipList(@PathVariable("deliveryPerson") String deliveryPerson) {
-        return new ApiResponse<List<BillOfLadingResponse>>(200, null,
-                billOfLadingService.getBillOfLadingByDeliveryPersonId(deliveryPerson));
-    }
+    // @GetMapping("/shipList/{deliveryPerson}")
+    // public ApiResponse<List<BillOfLadingResponse>>
+    // getShipList(@PathVariable("deliveryPerson") String deliveryPerson) {
+    // return new ApiResponse<List<BillOfLadingResponse>>(200, null,
+    // billOfLadingService.getBillOfLadingByDeliveryPersonId(deliveryPerson));
+    // }
 
     // @GetMapping("/billOfLadingList")
     // public ApiResponse<List<BillOfLadingResponse>> getAll() {
