484b6eff8ace7c4965d85f2f18405b97a4443027
thanhpvpd <thanhgavip04@gmail.com>
2024-07-03 20:20:59 +0700

    tạo bảng yêu cầu xác nhận chủ hàng

---
+++ src/main/java/store/chikendev/_2tm/entity/OwnerPermission.java
@@ -0,0 +1,47 @@
+package store.chikendev._2tm.entity;
+
+import java.util.Date;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+public class OwnerPermission {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Date createdAt;
+
+    @Column(length = 100)
+    private String bankName;
+
+    @Column(length = 100)
+    private String BankAccountNumber;
+
+    @Column(length = 100)
+    private String accountHolderName;
+
+    private Boolean status;
+
+    @OneToOne
+    @JoinColumn(name = "accountId")
+    private Account account;
+}
