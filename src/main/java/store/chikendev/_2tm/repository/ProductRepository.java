eb1f1de92d88e5c2ab0080c3a45614dbc94b802c
thanhpvpd <thanhgavip04@gmail.com>
2024-08-09 09:49:17 +0700

    fix lỗi hiện thị cả sản phẩm có TT 4

--- src/main/java/store/chikendev/_2tm/repository/ProductRepository.java
+++ src/main/java/store/chikendev/_2tm/repository/ProductRepository.java
@@ -14,7 +14,7 @@ import store.chikendev._2tm.entity.Store;
 
 @Repository
 public interface ProductRepository
-    extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
+        extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
     List<Product> findByQuantityGreaterThanOrderByCreatedAtDesc(int quantity);
 
     // @Query("select p from Product p where p.name like %:searchTerm% or
@@ -25,72 +25,49 @@ public interface ProductRepository
     // lấy sản phẩm đang bán để show lên trang chủ -
     // ** nếu laấy để làm chức năng khác thì tạo cái riêng
     // không thêm chức nang khác vào đâyủ
-    @Query(
-        "SELECT p FROM Product p WHERE p.quantity > 0 AND p.state.id = 2 ORDER BY p.createdAt DESC"
-    )
+    @Query("SELECT p FROM Product p WHERE p.quantity > 0 AND p.state.id = 2 ORDER BY p.createdAt DESC")
     Page<Product> findAvailableProducts(Pageable pageable);
 
-    @Query(
-        "SELECT p FROM Product p WHERE p.ownerId.id = :ownerId AND (:stateId IS NULL OR p.state.id = :stateId)"
-    )
+    @Query("SELECT p FROM Product p WHERE p.ownerId.id = :ownerId AND (:stateId IS NULL OR p.state.id = :stateId)")
     Page<Product> findConsignmentProductsByOwnerIdAndState(
-        @Param("ownerId") String ownerId,
-        @Param("stateId") Long stateId,
-        Pageable pageable
-    );
+            @Param("ownerId") String ownerId,
+            @Param("stateId") Long stateId,
+            Pageable pageable);
 
-    @Query(
-        "SELECT p FROM Product p WHERE p.quantity > 0 AND (p.state.id = 2 OR p.state.id = 4) AND (p.name LIKE :value OR p.description LIKE :value)"
-    )
+    @Query("SELECT p FROM Product p WHERE p.quantity > 0 AND p.state.id = 2 AND (p.name LIKE :value OR p.description LIKE :value)")
     Page<Product> findProductsBySearchTerm(
-        @Param("value") String value,
-        Pageable pageable
-    );
+            @Param("value") String value,
+            Pageable pageable);
 
-    @Query(
-        "SELECT p FROM Product p WHERE p.store = :store AND (:stateProduct IS NULL OR p.state.id = :stateProduct) AND p.type = :type"
-    )
+    @Query("SELECT p FROM Product p WHERE p.store = :store AND (:stateProduct IS NULL OR p.state.id = :stateProduct) AND p.type = :type")
     Page<Product> findConsignmentProductsByStoreAndState(
-        @Param("store") Store store,
-        @Param("stateProduct") Long stateProductId,
-        @Param("type") Boolean type,
-        Pageable pageable
-    );
+            @Param("store") Store store,
+            @Param("stateProduct") Long stateProductId,
+            @Param("type") Boolean type,
+            Pageable pageable);
 
-    @Query(
-        "SELECT p FROM Product p WHERE p.quantity > 0 AND p.category = :category AND (p.state.id = 2 OR p.state.id = 4)"
-    )
+    @Query("SELECT p FROM Product p WHERE p.quantity > 0 AND p.category = :category AND p.state.id = 2")
     Page<Product> findByPathCategory(
-        @Param("category") Category category,
-        Pageable pageable
-    );
+            @Param("category") Category category,
+            Pageable pageable);
 
     // nv xem san pham theo store
-    @Query(
-        "SELECT p FROM Product p WHERE p.store = :store  ORDER BY p.createdAt DESC"
-    )
+    @Query("SELECT p FROM Product p WHERE p.store = :store  ORDER BY p.createdAt DESC")
     Page<Product> findAllProductsInStore(
-        @Param("store") Store store,
-        Pageable pageable
-    );
+            @Param("store") Store store,
+            Pageable pageable);
 
-    // kh xem san pham theo store
-    @Query(
-        "SELECT p FROM Product p WHERE p.quantity > 0 AND p.store = :store AND (p.state.id = 2 OR p.state.id = 4)  ORDER BY p.createdAt DESC"
-    )
+    // kh xem san pham theo store và chi tiết store
+    @Query("SELECT p FROM Product p WHERE p.quantity > 0 AND p.store = :store AND p.state.id = 2 ORDER BY p.createdAt DESC")
     Page<Product> findAllProductsInStoreKH(
-        @Param("store") Store store,
-        Pageable pageable
-    );
+            @Param("store") Store store,
+            Pageable pageable);
 
-    @Query(
-        "SELECT p FROM Product p WHERE p.quantity > 0 AND (p.state.id = 2 OR p.state.id = 4) AND (:categoryId IS NULL OR p.category.id = :categoryId) AND (:storeId IS NULL OR p.store.id = :storeId) AND (:minPrice IS NULL OR p.price >= :minPrice) AND (:maxPrice IS NULL OR p.price <= :maxPrice) ORDER BY p.createdAt DESC"
-    )
+    @Query("SELECT p FROM Product p WHERE p.quantity > 0 AND p.state.id = 2 AND (:categoryId IS NULL OR p.category.id = :categoryId) AND (:storeId IS NULL OR p.store.id = :storeId) AND (:minPrice IS NULL OR p.price >= :minPrice) AND (:maxPrice IS NULL OR p.price <= :maxPrice) ORDER BY p.createdAt DESC")
     Page<Product> findProductByCondition(
-        @Param("categoryId") Long categoryId,
-        @Param("storeId") Long storeId,
-        @Param("minPrice") Long minPrice,
-        @Param("maxPrice") Long maxPrice,
-        Pageable pageable
-    );
+            @Param("categoryId") Long categoryId,
+            @Param("storeId") Long storeId,
+            @Param("minPrice") Long minPrice,
+            @Param("maxPrice") Long maxPrice,
+            Pageable pageable);
 }
