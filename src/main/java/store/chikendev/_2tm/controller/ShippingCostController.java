9bca217427abebaf55ebbc8adfd1a3260f036929
tuloc2904 <168943845+tuloc2904@users.noreply.github.com>
2024-08-12 16:35:11 +0700

    fix

--- src/main/java/store/chikendev/_2tm/controller/ShippingCostController.java
+++ src/main/java/store/chikendev/_2tm/controller/ShippingCostController.java
@@ -6,6 +6,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import store.chikendev._2tm.dto.request.ShippingCostRequest;
+import store.chikendev._2tm.dto.responce.ApiResponse;
 import store.chikendev._2tm.dto.responce.ShippingCostResponse;
 import store.chikendev._2tm.entity.ShippingCost;
 import store.chikendev._2tm.service.ShippingCostService;
@@ -16,23 +17,23 @@ public class ShippingCostController {
 
     @Autowired
     private ShippingCostService shippingCostService;
-    
+
     @PreAuthorize("hasAnyRole('ROLE_QTV')")
     @PostMapping("/create-ship-cost")
-    public ResponseEntity<ShippingCostResponse> createShippingCost(@RequestBody ShippingCostRequest request) {
+    public ApiResponse<ShippingCostResponse> createShippingCost(@RequestBody ShippingCostRequest request) {
         ShippingCost shippingCost = shippingCostService.createShippingCost(request.getCost());
         ShippingCostResponse response = new ShippingCostResponse();
         response.setId(shippingCost.getId());
         response.setCost(shippingCost.getCost());
-        return ResponseEntity.ok(response);
+        return new ApiResponse<ShippingCostResponse>(200, null, response);
     }
     @PreAuthorize("hasAnyRole('ROLE_QTV')")
     @PutMapping("/update-ship-cost/{id}")
-    public ResponseEntity<ShippingCostResponse> updateShippingCost(@PathVariable("id") Long id, @RequestBody ShippingCostRequest request) {
+    public ApiResponse<ShippingCostResponse> updateShippingCost(@PathVariable("id") Long id, @RequestBody ShippingCostRequest request) {
         ShippingCost shippingCost = shippingCostService.updateShippingCost(id, request.getCost());
         ShippingCostResponse response = new ShippingCostResponse();
         response.setId(shippingCost.getId());
         response.setCost(shippingCost.getCost());
-        return ResponseEntity.ok(response);
+        return new ApiResponse<ShippingCostResponse>(200, null, response);
     }
 }
