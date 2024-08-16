cad5c34dbc56a87b99ef6634abaac023de4414c3
hddev244 <daivvh244@gmail.com>
2024-08-16 12:37:03 +0700

    BillOfLading - complete -reject

--- src/main/java/store/chikendev/_2tm/entity/StateOrder.java
+++ src/main/java/store/chikendev/_2tm/entity/StateOrder.java
@@ -24,6 +24,7 @@ public class StateOrder {
     public static final Long IN_CONFIRM = 1L; //Đơn hàng đang chờ cửa hàng xác nhận và tạo vận 
     // cửa hàng xác nhận chờ vận chuyển
     public static final Long CONFIRMED = 2L; //đơn đã được cửa hàng xác nhận và tạo vận đơn, nhưng NVGH chưa lấy
+    public static final Long ON_REFECT = 4L; //khách hàng từ chối nhận hàng, đang vận chuyển về lại cửa hàng.
     public static final Long CANCELLED_ORDER = 13L; //Đơn hàng đã bị hủy
     public static final Long DELIVERING = 3L; // Đơn hàng đã được nhân viên lấy đi giao - trên dường giao cho khách hàng
     public static final Long DELIVERED_SUCCESS = 5L; //Người mua đã nhận hàng - giao hàng thành công
